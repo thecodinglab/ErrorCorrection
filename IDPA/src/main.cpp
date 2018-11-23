c759e8852f0e80b54893e55e0c05724ad645b78b
TheCodingLab <nairolf.retlaw@gmail.com>
2018-11-23 13:16:34 +0100

    Added random bit flip

--- IDPA/src/main.cpp
+++ IDPA/src/main.cpp
@@ -43,37 +43,50 @@ void benchmark(uint16_t (*algorithm)(uint16_t), const char * msg)
 	std::cout << "Took " << ms.count() << "ms for " << msg << std::endl;
 }
 
+void benchmarkAlgorithms()
+{
+	benchmark(&hamming_16_byte, "Florian's algorithm"); // Florian
+	benchmark(&hamming_15_4, "Matrix"); // Matrix
+	benchmark(&hamming_15_4_performance, "Matrix without for loop");
+	benchmark(&hamming_15_4_oneliner, "amazing oneliner");
+	benchmark(&hamming_matrix_asm_generate, "matrix using assembly");
+}
+
 int main()
 {
-    const char *string = "The quick brown fox jumps over the lazy dog";
+    //const char *string = "The quick brown fox jumps over the lazy dog";
 
     // CRC
     //std::cout << std::hex << Compute_CRC32_Simple((uint8_t*)string, strlen(string)) << std::endl;
     //std::cout << std::hex << crc<uint32_t, 24>(0x04C11DB7, string, strlen(string)) << std::endl;
 
+	srand((int) time(0));
 
-	//benchmark(&hamming_16); // Philip
-	benchmark(&hamming_16_byte, "Florian's algorithm"); // Florian
-	benchmark(&hamming_15_4, "Matrix"); // Matrix
-	benchmark(&hamming_15_4_performance, "Matrix without for loop");
-	benchmark(&hamming_15_4_oneliner, "amazing oneliner");
-    benchmark(&hamming_matrix_asm, "matrix using assembly");
+	while (1)
+	{
+		uint16_t input = 0b100111000110000;
+		uint16_t parity = hamming_matrix_asm_generate(input);
+		uint16_t message = input | (parity & 0xf);
 
-    //std::cout << std::bitset<16>(hamming_15_4_performance(0x4E20)) << std::endl;
+		uint32_t flippedBit = rand() % 15;
+		uint16_t received = input ^ (1 << flippedBit);
 
-    /*uint16_t data = 0x4E20;
-    uint16_t result = hamming_15_4(data);
+		uint16_t error = hamming_matrix_asm_check(received);
 
-    std::cout << std::bitset<16>(result) << "< solution" << std::endl;
-    std::cout << std::bitset<16>(hamming_15_4(result)) << "< check" << std::endl;
-    */
+		std::cout << std::hex;
+		std::cout << "Input: " << input << std::endl;
+		std::cout << "Parity: " << parity << std::endl;
+		std::cout << "Message: " << message << std::endl;
 
-    // Hamming-Code
-    // 1st letter: "T", UTF-8-code 84 = 1010100
-    /*std::cout << std::bitset<16>(84) << std::endl;
-    const uint16_t *data = hamming_16((uint8_t*)string, strlen(string));
-    std::cout << std::bitset<16>(data[0]) << std::endl;*/
+		std::cout << "Computed error: " << error << std::endl;
+		std::cout << "Real error: " << flippedBit << std::endl;
 
-    std::cin.get();
+		std::cout << std::endl;
+
+
+		//benchmarkAlgorithms();
+
+		std::cin.get();
+	}
     return 0;
 }
