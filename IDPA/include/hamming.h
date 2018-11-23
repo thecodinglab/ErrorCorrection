d7342d9e73dad110b0d143ce414569db2cc7e5eb
shadowsburney <33462587+shadowsburney@users.noreply.github.com>
2018-11-23 12:47:03 +0100

    Updated Generatormatrix

--- IDPA/include/hamming.h
+++ IDPA/include/hamming.h
@@ -18,6 +18,11 @@
 
 #include <stdint.h>
 
+#define MATRIX_ROW_1 0x7F80 /* 111111110000000 */
+#define MATRIX_ROW_2 0x7878 /* 111100001111000 */
+#define MATRIX_ROW_3 0x6666 /* 110011001100110 */
+#define MATRIX_ROW_4 0x5555 /* 101010101010101 */
+
 //template<typename T>
 static uint16_t* hamming_16(const void *buffer, size_t length)
 {
@@ -90,10 +95,10 @@ static uint16_t hamming_16_byte(uint16_t input) // Sorry, had to make it 16 bit 
 
 
 static uint16_t hamming_15_4(uint16_t data) {
-    uint16_t r1 = data & 0x6D58; // 110110101011000
-    uint16_t r2 = data & 0x5B34; // 101101100110100
-    uint16_t r3 = data & 0x38F2; // 011100011110010
-    uint16_t r4 = data & 0x7F1;  // 000011111110001
+    uint16_t r1 = data & MATRIX_ROW_1;
+    uint16_t r2 = data & MATRIX_ROW_2;
+    uint16_t r3 = data & MATRIX_ROW_3;
+    uint16_t r4 = data & MATRIX_ROW_4;
 
     uint16_t p1 = 0, p2 = 0, p3 = 0, p4 = 0;
     for (size_t i = 0; i < 15; i++) {
@@ -112,10 +117,10 @@ static uint16_t hamming_15_4(uint16_t data) {
 }
 
 static uint16_t hamming_15_4_performance(uint16_t data) {
-    uint16_t r1 = data & 0x6D58; // 110110101011000
-    uint16_t r2 = data & 0x5B34; // 101101100110100
-    uint16_t r3 = data & 0x38F2; // 011100011110010
-    uint16_t r4 = data & 0x7F1;  // 000011111110001
+    uint16_t r1 = data & MATRIX_ROW_1;
+    uint16_t r2 = data & MATRIX_ROW_2; 
+    uint16_t r3 = data & MATRIX_ROW_3;
+    uint16_t r4 = data & MATRIX_ROW_4;
 
     uint16_t p1 = 1 & ((r1 & 1) ^ ((r1 >> 1) & 1) ^ ((r1 >> 2) & 1) ^ ((r1 >> 3) & 1) ^ ((r1 >> 4) & 1) ^ ((r1 >> 5) & 1) ^ ((r1 >> 6) & 1) ^ ((r1 >> 7) & 1) ^ ((r1 >> 8) & 1) ^ ((r1 >> 9) & 1) ^ ((r1 >> 10) & 1) ^ ((r1 >> 11) & 1) ^ ((r1 >> 12) & 1) ^ ((r1 >> 13) & 1) ^ ((r1 >> 14) & 1));
     uint16_t p2 = 1 & ((r2 & 1) ^ ((r2 >> 1) & 1) ^ ((r2 >> 2) & 1) ^ ((r2 >> 3) & 1) ^ ((r2 >> 4) & 1) ^ ((r2 >> 5) & 1) ^ ((r2 >> 6) & 1) ^ ((r2 >> 7) & 1) ^ ((r2 >> 8) & 1) ^ ((r2 >> 9) & 1) ^ ((r2 >> 10) & 1) ^ ((r2 >> 11) & 1) ^ ((r2 >> 12) & 1) ^ ((r2 >> 13) & 1) ^ ((r2 >> 14) & 1));
@@ -128,27 +133,6 @@ static uint16_t hamming_15_4_performance(uint16_t data) {
 static uint16_t hamming_15_4_oneliner(uint16_t data) {
 	return data ^ ((1 & (((data & 0x6D58) & 1) ^ (((data & 0x6D58) >> 1) & 1) ^ (((data & 0x6D58) >> 2) & 1) ^ (((data & 0x6D58) >> 3) & 1) ^ (((data & 0x6D58) >> 4) & 1) ^ (((data & 0x6D58) >> 5) & 1) ^ (((data & 0x6D58) >> 6) & 1) ^ (((data & 0x6D58) >> 7) & 1) ^ (((data & 0x6D58) >> 8) & 1) ^ (((data & 0x6D58) >> 9) & 1) ^ (((data & 0x6D58) >> 10) & 1) ^ (((data & 0x6D58) >> 11) & 1) ^ (((data & 0x6D58) >> 12) & 1) ^ (((data & 0x6D58) >> 13) & 1) ^ (((data & 0x6D58) >> 14) & 1))) << 3) ^ ((1 & (((data & 0x5B34) & 1) ^ (((data & 0x5B34) >> 1) & 1) ^ (((data & 0x5B34) >> 2) & 1) ^ (((data & 0x5B34) >> 3) & 1) ^ (((data & 0x5B34) >> 4) & 1) ^ (((data & 0x5B34) >> 5) & 1) ^ (((data & 0x5B34) >> 6) & 1) ^ (((data & 0x5B34) >> 7) & 1) ^ (((data & 0x5B34) >> 8) & 1) ^ (((data & 0x5B34) >> 9) & 1) ^ (((data & 0x5B34) >> 10) & 1) ^ (((data & 0x5B34) >> 11) & 1) ^ (((data & 0x5B34) >> 12) & 1) ^ (((data & 0x5B34) >> 13) & 1) ^ (((data & 0x5B34) >> 14) & 1))) << 2) ^ ((1 & (((data & 0x38F2) & 1) ^ (((data & 0x38F2) >> 1) & 1) ^ (((data & 0x38F2) >> 2) & 1) ^ (((data & 0x38F2) >> 3) & 1) ^ (((data & 0x38F2) >> 4) & 1) ^ (((data & 0x38F2) >> 5) & 1) ^ (((data & 0x38F2) >> 6) & 1) ^ (((data & 0x38F2) >> 7) & 1) ^ (((data & 0x38F2) >> 8) & 1) ^ (((data & 0x38F2) >> 9) & 1) ^ (((data & 0x38F2) >> 10) & 1) ^ (((data & 0x38F2) >> 11) & 1) ^ (((data & 0x38F2) >> 12) & 1) ^ (((data & 0x38F2) >> 13) & 1) ^ (((data & 0x38F2) >> 14) & 1))) << 1) ^ (1 & (((data & 0x7F1) & 1) ^ (((data & 0x7F1) >> 1) & 1) ^ (((data & 0x7F1) >> 2) & 1) ^ (((data & 0x7F1) >> 3) & 1) ^ (((data & 0x7F1) >> 4) & 1) ^ (((data & 0x7F1) >> 5) & 1) ^ (((data & 0x7F1) >> 6) & 1) ^ (((data & 0x7F1) >> 7) & 1) ^ (((data & 0x7F1) >> 8) & 1) ^ (((data & 0x7F1) >> 9) & 1) ^ (((data & 0x7F1) >> 10) & 1) ^ (((data & 0x7F1) >> 11) & 1) ^ (((data & 0x7F1) >> 12) & 1) ^ (((data & 0x7F1) >> 13) & 1) ^ (((data & 0x7F1) >> 14) & 1)));
 }
-
-/*
-This one's 20-30 ms faster:
-
-static uint16_t hamming_15_4_performance(uint16_t data) {
-    uint16_t r1 = data & 0x6D58;
-    uint16_t r2 = data & 0x5B34;
-    uint16_t r3 = data & 0x38F2;
-    uint16_t r4 = data & 0x7F1;
-
-    return data ^ ((1 & ((r1 & 1) ^ ((r1 >> 1) & 1) ^ ((r1 >> 2) & 1) ^ ((r1 >> 3) & 1) ^ ((r1 >> 4) & 1) ^ ((r1 >> 5) & 1) ^ ((r1 >> 6) & 1) ^ ((r1 >> 7) & 1) ^ ((r1 >> 8) & 1) ^ ((r1 >> 9) & 1) ^ ((r1 >> 10) & 1) ^ ((r1 >> 11) & 1) ^ ((r1 >> 12) & 1) ^ ((r1 >> 13) & 1) ^ ((r1 >> 14) & 1))) << 3)
-    ^ ((1 & ((r2 & 1) ^ ((r2 >> 1) & 1) ^ ((r2 >> 2) & 1) ^ ((r2 >> 3) & 1) ^ ((r2 >> 4) & 1) ^ ((r2 >> 5) & 1) ^ ((r2 >> 6) & 1) ^ ((r2 >> 7) & 1) ^ ((r2 >> 8) & 1) ^ ((r2 >> 9) & 1) ^ ((r2 >> 10) & 1) ^ ((r2 >> 11) & 1) ^ ((r2 >> 12) & 1) ^ ((r2 >> 13) & 1) ^ ((r2 >> 14) & 1))) << 2)
-    ^ ((1 & ((r3 & 1) ^ ((r3 >> 1) & 1) ^ ((r3 >> 2) & 1) ^ ((r3 >> 3) & 1) ^ ((r3 >> 4) & 1) ^ ((r3 >> 5) & 1) ^ ((r3 >> 6) & 1) ^ ((r3 >> 7) & 1) ^ ((r3 >> 8) & 1) ^ ((r3 >> 9) & 1) ^ ((r3 >> 10) & 1) ^ ((r3 >> 11) & 1) ^ ((r3 >> 12) & 1) ^ ((r3 >> 13) & 1) ^ ((r3 >> 14) & 1))) << 1)
-    ^ (1 & ((r4 & 1) ^ ((r4 >> 1) & 1) ^ ((r4 >> 2) & 1) ^ ((r4 >> 3) & 1) ^ ((r4 >> 4) & 1) ^ ((r4 >> 5) & 1) ^ ((r4 >> 6) & 1) ^ ((r4 >> 7) & 1) ^ ((r4 >> 8) & 1) ^ ((r4 >> 9) & 1) ^ ((r4 >> 10) & 1) ^ ((r4 >> 11) & 1) ^ ((r4 >> 12) & 1) ^ ((r4 >> 13) & 1) ^ ((r4 >> 14) & 1)));
-}
-*/
-
-#define MATRIX_ROW_1 0x6D58 /* 0b110110101011000 */
-#define MATRIX_ROW_2 0x5B34 /* 0b101101100110100 */
-#define MATRIX_ROW_3 0x38F2 /* 0b011100011110010 */
-#define MATRIX_ROW_4 0x07F1 /* 0b000011111110001 */
 
 uint16_t hamming_matrix_asm(uint16_t in)
 {
